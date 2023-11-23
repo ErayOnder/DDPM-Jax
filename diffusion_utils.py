99432352b1a412aa26e0d4f8c90df5362d62e577
ErayOnder <erayonder21@ku.edu.tr>
2023-11-23 23:44:00 +0300

    Diffusion utilities started

---
+++ diffusion_utils.py
@@ -0,0 +1,21 @@
+import jax
+import jax.numpy as jnp
+import jax.random as random
+
+class SimpleDiffusion:
+    def __init__(self, timesteps=200):
+        self.timesteps = timesteps
+        self.initialize()
+    def initialize(self):
+        self.beta = jnp.linspace(10**(-4), 0.02, T)
+        self.alpha = 1 - self.beta
+        self.alpha_bar = jnp.cumprod(self.alpha)
+        self.sqrt_alpha_bar = jnp.sqrt(self.alpha_bar)
+        self.sqrt_one_minus_alpha_bar = jnp.sqrt(1 - self.alpha_bar)
+def forward_process(key, sd:SimpleDiffusion, x_0):
+    noise = random.normal(key, x_0.shape)
+    reshaped_sqrt_alpha_bar_t = jnp.reshape(jnp.take(sd.sqrt_alpha_bar, sd.timesteps), (-1, 1))
+    reshaped_sqrt_one_minus_alpha_bar_t = jnp.reshape(jnp.take(sd.sqrt_one_minus_alpha_bar, sd.timesteps), (-1, 1))
+    noisy_image = reshaped_sqrt_alpha_bar_t * x_0 + reshaped_sqrt_one_minus_alpha_bar_t * noise
+    return noisy_image, noise
+
--- input_pipeline.py
+++ input_pipeline.py
@@ -2,6 +2,10 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
 import matplotlib.pyplot as plt
+import jax.random as random
+
+from diffusion_utils import forward_process
+
 
 def get_dataset(dataset_name: str, split: str, shuffle: bool, batch_size: int, image_size: int):
     dataset = tfds.load(dataset_name, as_supervised=True, split=split, shuffle_files=shuffle)
@@ -11,6 +15,13 @@ def get_dataset(dataset_name: str, split: str, shuffle: bool, batch_size: int, i
     dataset = dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
     return dataset
 
+dataset_name = 'mnist'
+split = 'train'
+shuffle = True
+batch_size = 64
+image_size = 32
+dataset = get_dataset(dataset_name, split, shuffle, batch_size, image_size)
+
 def visualize_dataset(dataset, num_samples=5):
     images = next(iter(dataset.take(1)))
 
@@ -18,16 +29,21 @@ def visualize_dataset(dataset, num_samples=5):
 
     for i in range(num_samples):
         plt.subplot(1, num_samples, i + 1)
-        plt.imshow(rescaled_images[i])
+        plt.imshow(rescaled_images[i], cmap='gray')
         plt.axis('off')
     plt.show()
 
-dataset_name = 'celeb_a'
-split = 'train'
-shuffle = True
-batch_size = 256
-image_size = 32
-dataset = get_dataset(dataset_name, split, shuffle, batch_size, image_size)
-
 visualize_dataset(dataset)
 
+def visualize_noise(dataset):
+    sample_image = next(iter(dataset))[0]
+
+    for index, i in enumerate([1, 50, 100, 150, 200]):
+        noisy_image, noise = forward_process(random.PRNGKey(0), sample_image)
+        rescaled_image = ((noisy_image.numpy() + 1) / 2.0 * 255.0).astype(np.uint8)
+        plt.subplot(1, 6, index + 1)
+        plt.imshow(rescaled_image, cmap='gray')
+        plt.axis('off')
+    plt.show()
+
+visualize_noise(dataset)
